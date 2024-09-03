b0121fa0b796b0e10de1f28a08c10b4d4ac4c9b4
Diego Alfonso <124203169+diegoalf1710@users.noreply.github.com>
2024-09-03 16:47:49 -0600

    Scripts para uso futuro

---
+++ Assets/Scripts/AUDIO/VolumenSlider.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumenSlider : MonoBehaviour
+{
+
+    [SerializeField] Slider soundSlider;
+    [SerializeField] AudioMixer masterMixer;
+
+    private void Start()
+    {
+        SetVolume(PlayerPrefs.GetFloat("SavedVolumenMaestro", 50));
+    }
+
+    public void SetVolume(float _value)
+    {
+        if (_value < 1)
+        {
+            _value = .001f;
+        }
+        RefreshSlider(_value);
+        PlayerPrefs.SetFloat("SavedVolumenMaestro", _value);
+        masterMixer.SetFloat("VolumenMaestro", Mathf.Log10(_value / 100) * 20f);
+    }
+
+    public void SetVolumeFromSlider()
+    {
+        SetVolume(soundSlider.value);
+    }
+
+    public void RefreshSlider(float _value)
+    {
+        soundSlider.value = _value;
+    }
+}

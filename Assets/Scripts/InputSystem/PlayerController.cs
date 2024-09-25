a18f0567cea6ed1f30df663fb23b39b5256a9351
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-09-25 16:11:46 -0600

    TankControlsDone

--- Assets/Scripts/InputSystem/PlayerController.cs
+++ Assets/Scripts/InputSystem/PlayerController.cs
@@ -7,15 +7,33 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 playerInput;
     [SerializeField] CharacterController controller;
+    [SerializeField] float playerSpeed = 3;
+    [SerializeField] float playerRotation = 40;
     private void OnMove(InputValue value)
     {
         playerInput = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
+    private void PlayerMovement()
+    {
+        //Use the move method to move the player to the front and back
+        if(playerInput.y > 0)
+        {   
+            controller.Move(transform.forward * playerInput.y * playerSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(transform.forward * playerInput.y * (playerSpeed/2) * Time.deltaTime);
+        }
+       
+        
+        //Rotate the player
+        transform.Rotate(transform.up, playerRotation * playerInput.x * Time.deltaTime);
+    }
+
+    
     void Update()
     {
-        
-        
+        PlayerMovement();
     }
 }

ac914c678fda9a4a933105d5ca8f226225b85fa4
Diego Alfonso <124203169+diegoalf1710@users.noreply.github.com>
2024-10-23 15:18:14 -0600

    avances pausa

--- Assets/Scripts/MENUS/Pause.cs
+++ Assets/Scripts/MENUS/Pause.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject PausaMenu;
+    public GameObject PausaSettings;
+    public PlayerController player; 
+    public bool paused;
+    public string menuSceneName;
+
+    private void Start()
     {
-        
+       PausaMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            paused = !paused;
+            if(paused == true) 
+            {
+                player.enabled = false;  
+                PausaMenu.SetActive(true);             
+                Time.timeScale = 0;
+                //Cursor.visible = true;
+                //Cursor.lockState = CursorLockMode.None;
+                //AudioListener.pause = false;
+            }
+            if(paused == false)
+            {
+                player.enabled=true;
+                PausaMenu.SetActive(false);
+                Time.timeScale = 1;
+                //Cursor.visible = false;
+                //Cursor.lockState = CursorLockMode.Locked;
+                //AudioListener.pause = false;
+            }
+        }
+    }
+
+    public void resumeGame()
+    {
+        player.enabled = true;
+        PausaMenu.SetActive(false);
+        Time.timeScale=1;
+        //Cursor.visible = false;
+        //Cursor.lockState= CursorLockMode.Locked;
+        //AudioListener.pause= false;
+        paused = false;
+    }
+    public void backToMenu()
+    {
+        Time.timeScale = 1;
+        //Cursor.visible = true;
+        //Cursor.lockState = CursorLockMode.None;
+        //AudioListener.pause = false;
+        SceneManager.LoadScene(menuSceneName);
+        Debug.Log("Yendo A Menu");
+
+    }
+    public void quitGamePause()
+    {
+        Application.Quit();
+        Debug.Log("Pause Quitting...");
     }
 }

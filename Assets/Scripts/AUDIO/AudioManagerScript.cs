d2bcd024edba2cff4e831ec9a1087403247606d0
diegoalf1710 <124203169+diegoalf1710@users.noreply.github.com>
2024-11-17 21:57:04 -0600

    cosas

---
+++ Assets/Scripts/AUDIO/AudioManagerScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    public SonidoScript[] musicClips, sfxClips;
+    public AudioSource musicSource, sfxSource;
+}
---
+++ Assets/Scripts/AUDIO/SonidoScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SonidoScript
+{
+    public string nameAudio;
+    public AudioClip clip;
+}

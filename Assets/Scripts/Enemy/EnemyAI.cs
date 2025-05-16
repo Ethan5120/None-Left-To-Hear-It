814268a526b680bb9f2d929f9d27221dbe2b8a78
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2025-05-16 04:57:43 -0600

    Final Map Placed + Adjustments

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -366,7 +366,7 @@ public class EnemyAI : MonoBehaviour
     {
         attackTimer -= 1 * Time.deltaTime * managerData.gameTime;
         RaycastHit hit;
-        if(Physics.Raycast(new Vector3(transform.position.x, 2.5f, transform.position.z), transform.forward, out hit, 5f, playerLayer))
+        if(Physics.Raycast(new Vector3(transform.position.x, 1f, transform.position.z), transform.forward, out hit, 5f, playerLayer))
         {
             Debug.DrawRay(transform.position, transform.forward * hit.distance);
             Debug.Log("PlayerIsInFront");
@@ -467,7 +467,7 @@ void OnDrawGizmosSelected()
 {
         Gizmos.color = Color.red;
         Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
-        Gizmos.DrawRay(new Vector3(transform.position.x, 2.5f, transform.position.z), direction);
+        Gizmos.DrawRay(new Vector3(transform.position.x, 1f, transform.position.z), direction);
 }
 
 

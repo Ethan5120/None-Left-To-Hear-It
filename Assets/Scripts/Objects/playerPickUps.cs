a5d02a91e2cba0f733c812fc2a0137b5618b9604
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2025-08-06 13:59:05 -0600

    Localization Menus Progress

--- Assets/Scripts/Objects/playerPickUps.cs
+++ Assets/Scripts/Objects/playerPickUps.cs
@@ -24,7 +24,7 @@ public class playerPickUps : MonoBehaviour, I_Interactable
 
     [Header("UI Data")]
     [SerializeField] GameEvent turnOnPanel;
-    [SerializeField] [TextArea(3,5)]string TextToDisplay;
+    [TextArea(3,5)] public string TextToDisplay;
 
 
     void  Awake()
